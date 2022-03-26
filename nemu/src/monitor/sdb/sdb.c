899c8cb14bbaf794d8633e935933daab3557b123
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-26 19:25:10 +0800

    >  compile NEMU ysyx_22040237 吴岳峰 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:25:10 up  7:14,  1 user,  load average: 0.26, 0.18, 0.12

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -103,7 +103,7 @@ static int cmd_p(char *args){
   bool success;
   word_t val = expr(args,&success);
   if(success){
-    printf("%lu",val);
+    printf("%lu\n",val);
   }
   else {
   printf("expr rerurn false");
