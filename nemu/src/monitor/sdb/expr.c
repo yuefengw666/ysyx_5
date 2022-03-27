cbb546bca72ee6ca91711f59e019f8e4bcc1cd8d
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-27 17:17:15 +0800

    >  compile NEMU ysyx_22040237 吴岳峰 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:17:15 up  6:10,  1 user,  load average: 0.04, 0.04, 0.06

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -176,7 +176,7 @@ int get_main_op(int p, int q,bool *success){
   int max_priority = 7;
   for(i=p; i<=q; i++){
     if( (inprts == 0) && (tokens[i].priority < 7) ){
-      if(tokens[i].priority <= max_priority){
+      if(tokens[i].priority < max_priority){
         op_pos = i;
         printf("in get main op_pos:%d\n",op_pos);
         printf("tho op_pos prior: %d\n",tokens[i].priority);
