ec6d99cc27d81e4b21e0eae86e12ea1aca870dbc
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-28 18:34:33 +0800

    >  compile NEMU ysyx_22040237 吴岳峰 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:34:33 up  6:11,  1 user,  load average: 0.03, 0.13, 0.12

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -16,52 +16,83 @@ static char *code_format =
 "  return 0; "
 "}";
 
-int index = 0;
-#define l = 10000;
-static uint64_t choose(uint64_t n){
+//int idx;
+#define len 10
+static uint32_t choose(uint32_t n){
   return rand() % n;
 }
 
+static inline void gen_rand_expr(int l, int r) {
+    int op;
+    if (l == r)buf[l] = choose(10) + 48;
+    else if (r == l + 1) {
+        buf[l] = choose(9) + 1 + 48;
+        buf[r] = choose(10) + 48;
+    } else {
+        switch (choose(2)) {
+            case 0:
+                op = l + 1 + choose(r - l - 1);
+                gen_rand_expr(l, op - 1);
+                static char ops[] = "+-*/";
+                buf[op] = ops[choose(4)];
+                gen_rand_expr(op + 1, r);
+                break;
+            default:
+                buf[l] = '(';
+                buf[r] = ')';
+                gen_rand_expr(l + 1, r - 1);
+                break;
+        }
+    }
+}
+/*
 void gen_num(){
   char temp[15];
   sprintf(temp,"%d",rand()%101);
   for(int i=0; i <= strlen(temp); i++){
-    buf[++index] = temp[i];
+    buf[idx++] = temp[i];
+  }
+  if(strlen(temp)!=0){
+    buf[idx++] = 'U';
   }
 }
 
 void gen_prts(int type){
-  buf[++index] = type;
+  buf[idx++] = type;
 }
 
-void gen_space(){
+void gen_space(void){
   int num = choose(2);
   for(int i=0; i<=num; i++){
-    buf[++index] = ' ';
+    buf[idx++] = ' ';
   }
 }
 
-void gen_rand_op(){
+void gen_rand_op(void){
   switch(choose(4)){
-    case 0: buf[++index] = '+';break;
-    case 1: buf[++index] = '-';break;
-    case 2: buf[++index] = '*';break;
-    case 3: buf[++index] = '/';break;
+    case 0: buf[idx++] = '+';break;
+    case 1: buf[idx++] = '-';break;
+    case 2: buf[idx++] = '*';break;
+    case 3: buf[idx++] = '/';break;
   }
 }
 
 static inline void gen_rand_expr() {
-  buf[0] = '\0';
-  if(index > l){
-    
+  printf("in gen idx:%d\n",idx);
+  printf("buf:%d\n",buf[idx]);
+  //buf[0] = '\0';
+  if(idx > l){
+    //buf[idx++]='U';
+    gen_num();
     return;
   }
-  switch(choose(3)){
+  switch(choose(4)){
     case 0:gen_num(); gen_space();break;
     case 1:gen_prts('('); gen_space();gen_rand_expr(); gen_space();gen_prts(')');break;
     default:gen_rand_expr(); gen_space();gen_rand_op(); gen_space();gen_rand_expr(); break;
   }
 }
+*/
 
 int main(int argc, char *argv[]) {
   int seed = time(0);
@@ -72,8 +103,9 @@ int main(int argc, char *argv[]) {
   }
   int i;
   for (i = 0; i < loop; i ++) {
-    gen_rand_expr();
-
+    buf[0] = '\0';
+    gen_rand_expr(0,len);
+    //strcat(buf,"1 + 1");
     sprintf(code_buf, code_format, buf);
 
     FILE *fp = fopen("/tmp/.code.c", "w");
@@ -81,7 +113,7 @@ int main(int argc, char *argv[]) {
     fputs(code_buf, fp);
     fclose(fp);
 
-    int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
+    int ret = system("gcc -Werror /tmp/.code.c -o /tmp/.expr");
     if (ret != 0) continue;
 
     fp = popen("/tmp/.expr", "r");
@@ -91,7 +123,7 @@ int main(int argc, char *argv[]) {
     fscanf(fp, "%d", &result);
     pclose(fp);
 
-    printf("%u %s\n", result, buf);
+    printf("%d %s\n", result, buf);
   }
   return 0;
 }
