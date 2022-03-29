cfebb220adaf149cdd2c69ce0d19df5136dbe197
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-29 22:28:37 +0800

    >  compile NEMU ysyx_22040237 吴岳峰 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:28:37 up  1:23,  1 user,  load average: 0.04, 0.09, 0.09

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -16,10 +16,10 @@ int main(int argc, char *argv[]) {
   init_monitor(argc, argv);
 #endif
 
-  test_expr();
+  //test_expr();
 
   /* Start engine. */
-  //engine_start();
+  engine_start();
 
   return is_exit_status_bad();
 }
