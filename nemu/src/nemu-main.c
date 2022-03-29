24a8ebdff7719e777d34b9e3a9d32870e6741aa8
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-29 21:53:26 +0800

    >  compile NEMU ysyx_22040237 吴岳峰 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:53:26 up 47 min,  1 user,  load average: 0.07, 0.11, 0.09

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -1,9 +1,12 @@
 #include <common.h>
-
+#include "./monitor/sdb/sdb.h"
+#include <stdio.h>
 void init_monitor(int, char *[]);
 void am_init_monitor();
 void engine_start();
 int is_exit_status_bad();
+
+void test_expr();
 
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
@@ -13,8 +16,40 @@ int main(int argc, char *argv[]) {
   init_monitor(argc, argv);
 #endif
 
+  test_expr();
+
   /* Start engine. */
   engine_start();
 
   return is_exit_status_bad();
+}
+
+void test_expr(){
+  
+  FILE * fp=fopen("../tools/gen-expr/input","r");
+  char expression[70000];
+  char *s_temp;
+  char *str;
+  int cnt=0;
+  for(int i = 0;i < 1000;i++)
+  {
+    bool flag=true;
+    
+    expression[0]='\0';
+    s_temp = fgets(expression,70000,fp);
+    unsigned answer=0,ans;
+    str = strtok(expression," ");
+    sscanf(expression,"%u",&answer);
+    str=expression+strlen(expression)+1;
+    ans=expr(str,&flag);
+    if(answer!=ans)
+      printf("%d Wrong\n",i);
+    else {
+      cnt++;
+      printf("correct %d\n",i);
+    }
+  
+  }
+  printf("%s,\n",s_temp);
+  printf("correct number: %d\n",cnt);
 }
