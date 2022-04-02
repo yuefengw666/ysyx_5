edbf1ab0fb939aab556b1534d566f47fe40bd446
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-02 18:56:22 +0800

    >  compile NEMU ysyx_22040237 吴岳峰 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:56:22 up  7:45,  1 user,  load average: 0.29, 0.14, 0.10

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -25,12 +25,12 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #endif
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
-//#ifdef CONFIG_WATCHPOINT 
+#ifdef CONFIG_WATCHPOINT 
   bool changed;
   changed = scan_wp();
   if(changed)
     nemu_state.state = NEMU_STOP;
-//#endif
+#endif
 }
 
 static void exec_once(Decode *s, vaddr_t pc) {
