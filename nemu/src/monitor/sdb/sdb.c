482617e5dc63a8ca61c9ddf25b7a564616101b00
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-04 10:33:38 +0800

    >  compile NEMU ysyx_22040237 吴岳峰 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:33:38 up 23 min,  1 user,  load average: 0.14, 0.17, 0.16

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -107,7 +107,7 @@ static int cmd_p(char *args){
   bool success;
   word_t val = expr(args,&success);
   if(success){
-    printf("DEC:%ld\nHEX:0x%8lx\n",val,val);
+    printf("DEC:%ld\nHEX:0x%04lx\n",val,val);
   }
   else {
     printf("expr return false\n");
