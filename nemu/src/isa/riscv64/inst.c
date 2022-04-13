42343e88750011ae0fd8f4d20ab15a50dcb7bb95
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-13 23:50:32 +0800

    >  compile NEMU ysyx_22040237 吴岳峰 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:50:32 up  3:25,  1 user,  load average: 0.16, 0.14, 0.15

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -8,8 +8,8 @@
 #define Mw vaddr_write
 
 enum {
-  TYPE_I, TYPE_U, TYPE_S,
-  TYPE_N, TYPE_J// none
+  TYPE_R, TYPE_I, TYPE_U, TYPE_S,
+  TYPE_N, TYPE_J, TYPE_B// none
 };
 
 #define src1R(n) do { *src1 = R(n); } while (0)
@@ -18,6 +18,7 @@ enum {
 #define src1I(i) do { *src1 = i; } while (0)
 #define src2I(i) do { *src2 = i; } while (0)
 #define destI(i) do { *dest = i; } while (0)
+word_t offset=0;
 
 static word_t immI(uint32_t i) { return SEXT(BITS(i, 31, 20), 12); }
 static word_t immU(uint32_t i) { return SEXT(BITS(i, 31, 12), 20) << 12; }
@@ -27,8 +28,17 @@ static word_t immJ(uint32_t i) {
   word_t immJ_3 = BITS(i,30,21) << 1;
   word_t immJ_2 = BITS(i,20,20) << 11;
   word_t immJ_1 = BITS(i,19,12) << 12;
-  return SEXT((immJ_4 | immJ_1 | immJ_2 | immJ_3),20);
+  return SEXT((immJ_4 | immJ_1 | immJ_2 | immJ_3),21);
 }
+static word_t immB(uint32_t i) {
+  word_t immB_4 = BITS(i,31,31) << 12;
+  word_t immB_3 = BITS(i,30,25) << 5;
+  word_t immB_2 = BITS(i,11,8) << 1;
+  word_t immB_1 = BITS(i,7,7) << 11;
+  offset = SEXT((immB_4 | immB_1 | immB_3 | immB_2),13);
+  return offset;
+}
+
 static void decode_operand(Decode *s, word_t *dest, word_t *src1, word_t *src2, int type) {
   uint32_t i = s->isa.inst.val;
   int rd  = BITS(i, 11, 7);
@@ -36,10 +46,12 @@ static void decode_operand(Decode *s, word_t *dest, word_t *src1, word_t *src2, 
   int rs2 = BITS(i, 24, 20);
   destR(rd);
   switch (type) {
+    case TYPE_R: src1R(rs1); src2R(rs2);break;
     case TYPE_I: src1R(rs1);     src2I(immI(i)); break;
     case TYPE_U: src1I(immU(i)); break;
     case TYPE_S: destI(immS(i)); src1R(rs1); src2R(rs2); break;
     case TYPE_J: src1I(immJ(i));break;
+    case TYPE_B: src1R(rs1); src2R(rs2);immB(i);break; 
   }
 }
 
@@ -57,10 +69,19 @@ static int decode_exec(Decode *s) {
   INSTPAT("??????? ????? ????? ??? ????? 00101 11", auipc  , U, R(dest) = src1 + s->pc);
   INSTPAT("??????? ????? ????? 011 ????? 00000 11", ld     , I, R(dest) = Mr(src1 + src2, 8));
   INSTPAT("??????? ????? ????? 011 ????? 01000 11", sd     , S, Mw(src1 + dest, 8, src2));
+  //dummy
   INSTPAT("??????? ????? ????? 000 ????? 00100 11", addi   , I, R(dest) = src1 + src2);
   INSTPAT("??????? ????? ????? ??? ????? 11011 11", jal    , J, R(dest) = s->pc + 4, s->dnpc = s->pc + src1);
   INSTPAT("??????? ????? ????? 000 ????? 11001 11", jalr   , I, R(dest) = s->pc + 4, s->dnpc = src1 + src2);
-
+  //add
+  INSTPAT("??????? ????? ????? 000 ????? 11000 11", beq    , B, if(src1 == src2) s->dnpc = s->pc + offset);
+  INSTPAT("??????? ????? ????? 010 ????? 00000 11", lw     , I, R(dest) = SEXT(Mr(src1+src2,4),32) );//
+  INSTPAT("0000000 ????? ????? 000 ????? 01110 11", addw   , R, R(dest) = SEXT(BITS(src1 + src2,31,0),32) );
+  INSTPAT("0100000 ????? ????? 000 ????? 01100 11", sub    , R, R(dest) = src1 - src2 );
+  INSTPAT("??????? ????? ????? 011 ????? 00100 11", sltiu  , I, R(dest) =  (src1 < ((unsigned)src2)) ? 1 : 0 );//eq?
+  INSTPAT("??????? ????? ????? 001 ????? 11000 11", bne    , B, if(src1 != src2) s->dnpc = s->pc + offset);
+  INSTPAT("??????? ????? ????? 000 ????? 00110 11", addiw  , I, R(dest) = SEXT(BITS(src1 + src2,31,0),32) );
+  
 
   /*--------------------------------------------------------------------------------------------*/
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
