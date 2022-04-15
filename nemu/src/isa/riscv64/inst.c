6c93a5fccb0bbdbc17d52f7194a225cd73d0611b
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-15 21:22:57 +0800

    >  compile NEMU ysyx_22040237 吴岳峰 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:22:57 up 32 min,  1 user,  load average: 0.17, 0.16, 0.17

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -121,6 +121,7 @@ static int decode_exec(Decode *s) {
   //shift
   INSTPAT("0000000 ????? ????? 101 ????? 00110 11", srliw  , I, if(BITS(src2,5,5)==0) R(dest) = SEXT(BITS((unsigned)src1 >> BITS(src2,5,0),31,0),32) );
   INSTPAT("0100000 ????? ????? 101 ????? 01110 11", sraw   , R, R(dest) = SEXT(((signed)BITS(src1,31,0) >> BITS(src2,4,0)),32) );
+  INSTPAT("0000000 ????? ????? 101 ????? 01110 11", srlw   , R, R(dest) = SEXT(((unsigned)BITS(src1,31,0) >> BITS(src2,4,0)),32) );
   
 
 
