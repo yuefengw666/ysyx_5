30347143c816512b5952169550e43175ef7863f3
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-19 23:27:49 +0800

    >  compile NEMU ysyx_22040237 吴岳峰 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:27:49 up  2:40,  1 user,  load average: 0.01, 0.08, 0.08

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,27 +5,70 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  //panic("Not implemented");
+  size_t len = 0;
+  while(s[len] != '\0') len++;
+  return len;
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  //panic("Not implemented");
+  size_t i;
+  for (i = 0; src[i] != '\0'; i++) dst[i] = src[i];
+  
+  dst[i] = '\n';
+
+  return dst;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+  //panic("Not implemented");
+  size_t i;
+
+  for (i = 0; i < n && src[i] != '\0'; i++)
+    dst[i] = src[i];
+  for ( ; i < n; i++)
+    dst[i] = '\0';
+  
+  return dst;
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  //panic("Not implemented");
+  size_t src_len = 0;
+  size_t dst_len = 0;
+  while(src[src_len]!='\0') src_len++;
+  while(dst[dst_len]!='\0') dst_len++;
+
+  size_t i;
+  for(i=0; src[i]!='\0'&& i<src_len; i++)
+    dst[dst_len+i] = src[i];
+  dst[dst_len+i] = '\0';
+
+  return dst;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  //panic("Not implemented");
+  size_t i;
+  for(i=0; s1[i]==s2[i]; i++){
+    if(s1[i] == '\0') return 0;
+  }
+  if(s1[i] > s2[i]) return 1;
+  else return -1;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+  //panic("Not implemented");
+  while(*s1==*s2 && *s1!='\0' && *s2!='\0' && (n--)){
+    s1++;
+    s2++;
+  }
+
+  if(n==0 || *s1==*s2) return 0;
+  else if(*s1 > *s2) return 1;
+  else return -1;
+
 }
 
 void *memset(void *s, int c, size_t n) {
