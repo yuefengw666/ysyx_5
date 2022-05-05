39e9534bc69b2ba7cb672e8b3730e25f797bef61
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-05 18:17:09 +0800

    >  compile NEMU ysyx_22040237 吴岳峰 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:17:09 up 23:31,  1 user,  load average: 1.62, 1.61, 1.55

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -151,6 +151,7 @@ void cpu_exec(uint64_t n) {
   }
 
   uint64_t timer_start = get_time();
+  printf("error befor ringbuf_int!\n");
   ringbuf_int(iring,iringbuf,IRB_SIZE);
   execute(n);
  //add some inst after bad inst;
