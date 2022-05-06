e6830918222bed3bb9b492f0194cfa82fec36d37
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-06 13:44:18 +0800

    >  compile NEMU ysyx_22040237 吴岳峰 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:44:18 up  2:45,  1 user,  load average: 0.68, 0.50, 0.26

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -14,7 +14,7 @@
 
 #define IRB_SIZE 128
 #define IRB_LENGTH 128
-typedef struct 
+/*typedef struct 
 {
   char (*buf)[128];
   unsigned int size;
@@ -35,24 +35,24 @@ int ringbuf_int(RINGBUF *ringbuf, char (*bufptr)[128], unsigned int ibr_size){
 
   return 0;
 }
-/*
+
 int ringbuf_free(RINGBUF *ringbuf){
   free(ringbuf->buf);
   return 0;
 }
-*/
+
 int ringbuf_push(RINGBUF *ringbuf, char *str_in){
   //ringbuf->buf += snprintf(ringbuf->buf, IRB_LENGTH, "%s", str_in);
   memcpy(ringbuf->buf[ringbuf->tail], str_in, IRB_LENGTH);
   ringbuf->tail = (ringbuf->tail+1) % ringbuf->size;
   return 0;
 }
-/*
+
 int ringbuf_poll(RINGBUF *ringbuf, char str_out){
   str_out;
 }
 */
-
+/*
 void ringbuf_display(RINGBUF *ringbuf){
   unsigned head = ringbuf->head;
   unsigned tail = ringbuf->tail;
@@ -63,11 +63,12 @@ void ringbuf_display(RINGBUF *ringbuf){
     head = (head + 1) % size;
   }
   return;
-}
-
+}*/
+/*
 char iringbuf[IRB_SIZE][IRB_LENGTH]={};
-RINGBUF *iring;
-
+RINGBUF ir, *iring;
+iring = &ir;
+*/
 CPU_state cpu = {};
 uint64_t g_nr_guest_inst = 0;
 static uint64_t g_timer = 0; // unit: us
@@ -79,9 +80,6 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #ifdef CONFIG_ITRACE_COND
   if (ITRACE_COND) { 
     log_write("%s\n", _this->logbuf);
-    //printf("%d\n",_this->logbuf[1]);
-    //printf("error!\n"); 
-    ringbuf_push(iring,_this->logbuf);
   }//add some inst befer bad inst
 #endif
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
@@ -157,9 +155,7 @@ void cpu_exec(uint64_t n) {
   }
 
   uint64_t timer_start = get_time();
-  printf("error befor ringbuf_int!\n");
-  ringbuf_int(iring, iringbuf, IRB_SIZE);
-  printf("after ringbuf_int\n");
+  
   execute(n);
  //add some inst after bad inst;
   uint64_t timer_end = get_time();
@@ -169,7 +165,6 @@ void cpu_exec(uint64_t n) {
     case NEMU_RUNNING: nemu_state.state = NEMU_STOP; break;
 
     case NEMU_END: case NEMU_ABORT:
-      ringbuf_display(iring);
       Log("nemu: %s at pc = " FMT_WORD,
           (nemu_state.state == NEMU_ABORT ? ASNI_FMT("ABORT", ASNI_FG_RED) :
            (nemu_state.halt_ret == 0 ? ASNI_FMT("HIT GOOD TRAP", ASNI_FG_GREEN) :
