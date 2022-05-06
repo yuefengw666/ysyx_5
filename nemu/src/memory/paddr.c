d29caddec666e09bdc5b5e0fd510374e0ce2ddee
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-06 14:22:25 +0800

    >  compile NEMU ysyx_22040237 吴岳峰 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:22:25 up  3:23,  1 user,  load average: 0.76, 0.74, 0.57

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -43,6 +43,12 @@ void init_mem() {
 }
 
 word_t paddr_read(paddr_t addr, int len) {
+  #ifdef CONFIG_MTRACE
+    if(likely(in_pmem(addr))){
+      word_t data_rd = pmem_read(addr, len);
+      Log("MTRACE-READ:addr:%x,data:%x,bytes:%d\n",addr,data_rd,len);
+    }
+  #endif
   if (likely(in_pmem(addr))) return pmem_read(addr, len);
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
   out_of_bound(addr);
@@ -50,6 +56,12 @@ word_t paddr_read(paddr_t addr, int len) {
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
+  #ifdef CONFIG_MTRACE
+    if(likely(in_pmem(addr))){
+      word_t data_wr = pmem_write(addr, len);
+      Log("MTRACE-WRITE:addr:%x,data:%x,bytes:%d\n",addr,data_wr,len);
+    }
+  #endif
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
