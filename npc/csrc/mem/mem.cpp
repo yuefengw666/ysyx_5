607768608a3b01c5881158e97bbfa7be9f2cd6d6
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-14 22:10:52 +0800

    >  sim RTL ysyx_22040237 吴岳峰 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:10:52 up 12:24,  1 user,  load average: 2.17, 1.81, 1.55

--- npc/csrc/mem/mem.cpp
+++ npc/csrc/mem/mem.cpp
@@ -19,17 +19,17 @@ word_t pmem_read(paddr_t addr){
     if( (addr >= CONFIG_MBASE) && (addr < (paddr_t)CONFIG_MBASE + CONFIG_MSIZE) ){
       printf("in pmem_read\n");
       word_t ret = host_read(guest_to_host(addr));
-      printf("read data: %lx\n",ret);
+      printf("read data: %x\n",ret);
       return ret;
     }
-    printf("read address = %lx is out of bound of pmem.",addr);
+    printf("read address = %x is out of bound of pmem.",addr);
     return 0;
 }
 
 void pmem_write(paddr_t addr, word_t data){
     if( (addr >= CONFIG_MBASE) && (addr < (paddr_t)CONFIG_MBASE + CONFIG_MSIZE) ){
         printf("in pmem_write\n");
-        printf("write data : %x \n",data);
+        printf("write data : %lx \n",data);
         host_write(guest_to_host(addr), data);
         return;
     }
