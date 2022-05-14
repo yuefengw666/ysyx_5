ebceb6685438d174f476e929bd668e5c24811afb
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-14 21:46:45 +0800

    >  sim RTL ysyx_22040237 吴岳峰 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:46:45 up 12:00,  1 user,  load average: 1.60, 1.22, 1.38

--- npc/csrc/include/mem.h
+++ npc/csrc/include/mem.h
@@ -1,6 +1,7 @@
 #include "npc_common.h"
 
 uint8_t* guest_to_host(paddr_t paddr);
-
+static inline word_t host_read(void *addr);
+static inline void host_write(void *addr, word_t data);
 word_t pmem_read(paddr_t addr);
 void pmem_write(paddr_t addr, word_t data);
