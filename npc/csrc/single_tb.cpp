a0ab9ad21606770dbebc11f32c6ffd9d557dccc1
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-15 15:22:22 +0800

    >  sim RTL ysyx_22040237 吴岳峰 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:22:22 up  3:31,  1 user,  load average: 2.71, 2.00, 1.74

--- npc/csrc/single_tb.cpp
+++ npc/csrc/single_tb.cpp
@@ -33,7 +33,7 @@ void ebreak(){
     printf("1\n");
     delete dut;
     printf("2\n");
-    exit(-1));
+    exit(-1);
 }
 
 void dut_reset(Vysyx_22040237_rv_single_cyc_cpu_top *dut, vluint64_t &posedge_cnt){
