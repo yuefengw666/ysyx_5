bf9a011c802b968fd81a5ffa60beb7e141af969d
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-15 18:12:19 +0800

    >  sim RTL ysyx_22040237 吴岳峰 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:12:19 up  6:21,  1 user,  load average: 2.41, 1.86, 1.53

--- npc/csrc/mem/mem.cpp
+++ npc/csrc/mem/mem.cpp
@@ -10,22 +10,14 @@ static inline word_t host_read(void *addr){
 }
 
 static inline void host_write(void *addr, word_t data){
-    printf("in host _write data:%lx\n",data);
     *(uint64_t *)addr = data;
-    printf("write data:%lx\n",*((uint64_t *)addr));
     return;
 }
 
 word_t pmem_read(paddr_t addr){
     if( (addr >= CONFIG_MBASE) && (addr < (paddr_t)CONFIG_MBASE + CONFIG_MSIZE) ){
-      printf("in pmem_read,addr=%x\n",addr);
-      //word_t *ret = *(host_read(guest_to_host(addr)));
-      //printf("read data: %lx\n",ret);
-
       word_t ret = host_read(guest_to_host(addr));
-      printf("read data ret; %lx\n",ret);
       return ret;
-      //return host_read(guest_to_host(addr));
     }
     printf("read address = %x is out of bound of pmem.",addr);
     return 0;
@@ -33,8 +25,6 @@ word_t pmem_read(paddr_t addr){
 
 void pmem_write(paddr_t addr, word_t data){
     if( (addr >= CONFIG_MBASE) && (addr < (paddr_t)CONFIG_MBASE + CONFIG_MSIZE) ){
-        printf("in pmem_write\n");
-        printf("write data : %lx \n",data);
         host_write(guest_to_host(addr), data);
         return;
     }
