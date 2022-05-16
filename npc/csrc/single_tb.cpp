47bce380782227abd032d9d6e0d094d6eb621225
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-16 21:50:50 +0800

    >  sim RTL ysyx_22040237 吴岳峰 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:50:50 up  4:01,  1 user,  load average: 0.21, 0.12, 0.05

--- npc/csrc/single_tb.cpp
+++ npc/csrc/single_tb.cpp
@@ -42,6 +42,42 @@ void dut_reset(Vysyx_22040237_rv_single_cyc_cpu_top *dut, vluint64_t &posedge_cn
     }
 }
 
+static char *img_file = NULL;
+long img_size = 0;
+
+static long load_img() {
+  if (img_file == NULL) {
+    Log("No image is given. Use the default build-in image.");
+    return 4096; // built-in image size
+  }
+
+  FILE *fp = fopen(img_file, "rb");
+  Assert(fp, "Can not open '%s'", img_file);
+
+  fseek(fp, 0, SEEK_END);
+  long size = ftell(fp);
+
+  Log("The image is %s, size = %ld", img_file, size);
+
+  fseek(fp, 0, SEEK_SET);
+  //int ret = fread(guest_to_host(RESET_VECTOR), size, 1, fp);
+  int ret = fread(pmem, size, 1, fp);
+  assert(ret == 1);
+
+  fclose(fp);
+  return size;
+}
+
+int parse_args(int argc, char *argv[]){
+    if(argc == 2){
+        if(strlen(argc[1] != 0)){
+            img_file = argv[1];
+            img_size = load_img();
+        }
+    }
+    return 0;
+}
+
 int main(int argc, char**argv, char** env){
 
     pmem_write(0x80000000,(imm+1<<20) | (rs1<<15) | (funt3<<12) | (rd<<7) | opcode_addi);
