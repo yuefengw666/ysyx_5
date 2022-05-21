9f83d34ba3093518faf5a6ee5f320626b95b3b07
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-21 22:31:06 +0800

    >  sim RTL ysyx_22040237 吴岳峰 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:31:06 up 13:00,  1 user,  load average: 0.73, 0.23, 0.08

--- npc/csrc/single_tb.cpp
+++ npc/csrc/single_tb.cpp
@@ -71,10 +71,11 @@ void ebreak(){
     int npc_exit_flag = 0;
     if(npc_cpu.gpr[10] != 0){
         //printf("regs[10]:%lx\n",npc_cpu.gpr[10]);
-        npc_exit_flag = 0;
+        npc_exit_flag = 1;
         printf("npc: %s at pc = %x\n",ASNI_FMT("HIT BAD TRAP", ASNI_FG_RED),dut->pc); 
     }
     else {
+        npc_exit_flag = 1;
         printf("npc: %s at pc = %x\n",ASNI_FMT("HIT GOOD TRAP", ASNI_FG_GREEN), dut->pc);
     }
     exit_npc(npc_exit_flag);
