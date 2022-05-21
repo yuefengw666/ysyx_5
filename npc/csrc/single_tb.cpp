05677aa344ed708de0f14462311b45274423a0d5
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-21 22:45:24 +0800

    >  sim RTL ysyx_22040237 吴岳峰 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:45:24 up 13:14,  1 user,  load average: 0.64, 0.67, 0.54

--- npc/csrc/single_tb.cpp
+++ npc/csrc/single_tb.cpp
@@ -132,7 +132,7 @@ void npc_exec(uint64_t n){
         case NPC_RUNNING: npc_state.state = NPC_STOP;break;
         case NPC_ABORT:
         case NPC_END:
-            printf("npc: %s, invalid opcode(%x) at pc = %lx\n", ASNI_FMT("ABORT", ASNI_BG_RED),dut->inst_in,npc_state.halt_pc);
+            printf("npc: %s, invalid opcode at pc = %lx\n", ASNI_FMT("ABORT", ASNI_BG_RED),npc_state.halt_pc);
                 //(npc_state.state == NEMU_ABORT ? ASNI_FMT("ABORT", ASNI_FG_RED) :
                 //(npc_state.halt_ret == 0 ? ASNI_FMT("HIT GOOD TRAP", ASNI_FG_GREEN) :
                 //ASNI_FMT("HIT BAD TRAP", ASNI_FG_RED))),
