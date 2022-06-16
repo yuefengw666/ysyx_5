722292cb0502cd37224ed2e068bb5ed94ae50d67
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-16 21:20:44 +0800

    >  compile NEMU ysyx_22040237 吴岳峰 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:20:44 up 43 min,  1 user,  load average: 0.73, 0.36, 0.36

--- nemu/src/isa/riscv64/difftest/dut.c
+++ nemu/src/isa/riscv64/difftest/dut.c
@@ -4,7 +4,10 @@
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   for(int i=0; i<32; i++){
-    if(ref_r->gpr[i] != cpu.gpr[i]) return false;
+    if(ref_r->gpr[i] != cpu.gpr[i]) {
+       
+      return false;
+    }
   }
 
   if(ref_r->pc != cpu.pc) return false;
