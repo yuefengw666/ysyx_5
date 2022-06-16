25de8fd60aa7e613279839d0a22bc3286882523c
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-16 21:53:06 +0800

    >  compile NEMU ysyx_22040237 吴岳峰 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:53:06 up  1:15,  1 user,  load average: 0.94, 0.71, 0.52

--- nemu/src/isa/riscv64/difftest/dut.c
+++ nemu/src/isa/riscv64/difftest/dut.c
@@ -7,8 +7,9 @@ extern const char* regs[];
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   for(int i=0; i<32; i++){
     if(ref_r->gpr[i] != cpu.gpr[i]) {
-      printf("%s---ref:%08lx",regs[i],ref_r->gpr[i]);
-      printf("\tnemu:%08lx\n",cpu.gpr[i]);
+      printf("Difftest %s at gpr:%s!",ASNI_FMT("E*-->",ASNI_FG_RED),regs[i]);
+      printf("%s:%08lx\n",ASNI_FMT("REF",ASNI_FG_GREEN),ref_r->gpr[i]);
+      printf("%s:%08lx\n",ASNI_FMT("NEMU",ASNI_FG_RED),cpu.gpr[i]);
       return false;
     }
   }
