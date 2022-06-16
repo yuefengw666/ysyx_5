395571cc746c2085ff32d7dc228e4a080ee02b55
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-16 21:12:29 +0800

    >  compile NEMU ysyx_22040237 吴岳峰 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:12:29 up 35 min,  1 user,  load average: 0.75, 0.50, 0.44

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -4,15 +4,37 @@
 #include <memory/paddr.h>
 
 void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
-  assert(0);
+  //assert(0);
+  if(direction == DIFFTEST_TO_DUT){
+    buf = (void *)guest_to_host(addr);
+  }
+  else if(direction == DIFFTEST_TO_REF){
+    Log("%x,%lx", addr, n);
+    char *buf_char = (char *)buf;
+    for (int i = 0; i < n; i++)
+    {
+      paddr_write(addr + i, 1, buf_char[i]);
+    }
+  }
 }
 
 void difftest_regcpy(void *dut, bool direction) {
-  assert(0);
+  //assert(0);
+    if (direction == DIFFTEST_TO_DUT)
+  {
+    for (int i = 0; i < 32; i++)
+      ((CPU_state *)dut)->gpr[i] = cpu.gpr[i];
+    ((CPU_state *)dut)->pc = cpu.pc;
+  }
+  else if (direction == DIFFTEST_TO_REF)
+  {
+    cpu = *(CPU_state *)dut;
+  }
 }
 
 void difftest_exec(uint64_t n) {
-  assert(0);
+  //assert(0);
+  cpu_exec(n);
 }
 
 void difftest_raise_intr(word_t NO) {
