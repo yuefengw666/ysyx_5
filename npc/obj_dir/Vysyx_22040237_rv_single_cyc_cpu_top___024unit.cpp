7edf22a71086e4e8f3e1a203f5dbf3d188fb4ff6
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-21 16:00:24 +0800

    >  sim RTL ysyx_22040237 吴岳峰 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:00:24 up  6:29,  1 user,  load average: 1.18, 0.95, 0.73

--- npc/obj_dir/Vysyx_22040237_rv_single_cyc_cpu_top___024unit.cpp
+++ npc/obj_dir/Vysyx_22040237_rv_single_cyc_cpu_top___024unit.cpp
@@ -17,18 +17,18 @@ VL_INLINE_OPT void Vysyx_22040237_rv_single_cyc_cpu_top___024unit____Vdpiimwrap_
     ebreak();
 }
 
-extern "C" void set_npc_state(int state, const svLogicVecVal* pc, int halt_ret);
+extern "C" void set_npc_state(int state, const svOpenArrayHandle pc, int halt_ret);
 
-VL_INLINE_OPT void Vysyx_22040237_rv_single_cyc_cpu_top___024unit____Vdpiimwrap_set_npc_state_TOP____024unit(IData/*31:0*/ state, QData/*63:0*/ pc, IData/*31:0*/ halt_ret) {
-    VL_DEBUG_IF(VL_DBG_MSGF("+        Vysyx_22040237_rv_single_cyc_cpu_top___024unit____Vdpiimwrap_set_npc_state_TOP____024unit\n"); );
+VL_INLINE_OPT void Vysyx_22040237_rv_single_cyc_cpu_top___024unit____Vdpiimwrap_set_npc_state__Vdpioc2_TOP____024unit(IData/*31:0*/ state, const QData/*63:0*/ &pc, IData/*31:0*/ halt_ret) {
+    VL_DEBUG_IF(VL_DBG_MSGF("+        Vysyx_22040237_rv_single_cyc_cpu_top___024unit____Vdpiimwrap_set_npc_state__Vdpioc2_TOP____024unit\n"); );
     // Body
     int state__Vcvt;
     for (size_t state__Vidx = 0; state__Vidx < 1; ++state__Vidx) state__Vcvt = state;
-    svLogicVecVal pc__Vcvt[2];
-    for (size_t pc__Vidx = 0; pc__Vidx < 1; ++pc__Vidx) VL_SET_SVLV_Q(64, pc__Vcvt + 2 * pc__Vidx, pc);
+    static const VerilatedVarProps pc__Vopenprops(VLVT_UINT64, VLVD_IN, VerilatedVarProps::Packed(), 63, 0);
+    VerilatedDpiOpenVar pc__Vopenarray (&pc__Vopenprops, &pc);
     int halt_ret__Vcvt;
     for (size_t halt_ret__Vidx = 0; halt_ret__Vidx < 1; ++halt_ret__Vidx) halt_ret__Vcvt = halt_ret;
-    set_npc_state(state__Vcvt, pc__Vcvt, halt_ret__Vcvt);
+    set_npc_state(state__Vcvt, &pc__Vopenarray, halt_ret__Vcvt);
 }
 
 extern "C" void set_gpr_ptr(const svOpenArrayHandle a);
--- npc/vsrc/single_cyc/ysyx_22040237_exu.v
+++ npc/vsrc/single_cyc/ysyx_22040237_exu.v
@@ -1,5 +1,5 @@
 import "DPI-C" function void ebreak();
-import "DPI-C" function void set_npc_state(int state, input logic [63:0] pc[], int halt_ret);
+import "DPI-C" function void set_npc_state(int state, int pc, int halt_ret);
 module ysyx_22040237_exu(
   input clk,
   input rst,
