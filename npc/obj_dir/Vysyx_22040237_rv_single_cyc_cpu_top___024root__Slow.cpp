a2fb63ee512297fa8527eb9b200f79ccdaebb82b
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-21 10:28:48 +0800

    >  sim RTL ysyx_22040237 吴岳峰 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:28:48 up 58 min,  1 user,  load average: 0.75, 0.57, 0.34

--- npc/obj_dir/Vysyx_22040237_rv_single_cyc_cpu_top___024root__Slow.cpp
+++ npc/obj_dir/Vysyx_22040237_rv_single_cyc_cpu_top___024root__Slow.cpp
@@ -54,7 +54,7 @@ void Vysyx_22040237_rv_single_cyc_cpu_top___024root___settle__TOP__1(Vysyx_22040
     vlSelf->ysyx_22040237_rv_single_cyc_cpu_top__DOT__ysyx_22040237_idu_u0__DOT__inst_addi 
         = (IData)((0x13U == (0x707fU & vlSelf->inst_in)));
     vlSelf->ysyx_22040237_rv_single_cyc_cpu_top__DOT__ysyx_22040237_idu_u0__DOT__inst_lui 
-        = (IData)((0x37U == (0x7fU & vlSelf->inst_in)));
+        = (IData)((0x36U == (0x7fU & vlSelf->inst_in)));
     vlSelf->ysyx_22040237_rv_single_cyc_cpu_top__DOT__ysyx_22040237_idu_u0__DOT__inst_auipc 
         = (IData)((0x17U == (0x7fU & vlSelf->inst_in)));
     vlSelf->ysyx_22040237_rv_single_cyc_cpu_top__DOT__ysyx_22040237_idu_u0__DOT__inst_jal 
