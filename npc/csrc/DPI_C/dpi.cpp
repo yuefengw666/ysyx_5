44eb8f6de199cca168145355393b56157ee5352e
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-21 16:36:23 +0800

    >  sim RTL ysyx_22040237 吴岳峰 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:36:23 up  7:05,  1 user,  load average: 1.26, 1.26, 1.02

--- npc/csrc/DPI_C/dpi.cpp
+++ npc/csrc/DPI_C/dpi.cpp
@@ -33,7 +33,7 @@ void ebreak(){
 }
 
 void invalid_inst_o(){
-  printf("invalid opcode at pc = %08lx\n",npc_cpu.pc);
+  printf("%s at pc = %08lx\n",ASNI_FMT("invalid opcode", ASNI_FG_RED),npc_cpu.pc);
 
   printf("There are two cases which will trigger this unexpected exception:\n"
       "1. The instruction at PC = %08lx is not implemented.\n"
