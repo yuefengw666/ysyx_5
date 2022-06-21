8c142bc9418390cbf58aac0bb29064e8fac4a68d
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-21 23:13:21 +0800

    >  compile NEMU ysyx_22040237 吴岳峰 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:13:21 up  1:48,  1 user,  load average: 0.70, 0.78, 0.72

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -30,17 +30,18 @@ static void iringbuf_wr(char *data_wr){
 }
 
 static void iringbuf_display(){
+  printf("%s\n",ASNI_FMT("Error instruction apper in ...",ASNI_FG_RED));
   int error_inst_pos = ( tail - 1 ) % IRB_SIZE; 
   if( tail < (IRB_SIZE + 1) ){
     for(int i=0; i<= (tail-1); i++){
-      if(i == error_inst_pos) printf("%s%s\n", ASNI_FMT("E*-->",ASNI_FG_RED),iringbuf[i]);
-      else printf("     %s\n",iringbuf[i]);
+      if(i == error_inst_pos) printf("%s%s\n", ASNI_FMT("E*->",ASNI_FG_RED),iringbuf[i]);
+      else printf("    %s\n",iringbuf[i]);
     }
   }
   else{
     for(int j=0; j<=IRB_SIZE; j++){
-      if(j == error_inst_pos) printf("%s%s\n", ASNI_FMT("E*-->",ASNI_FG_RED),iringbuf[j]);
-      else printf("     %s\n",iringbuf[j]);
+      if(j == error_inst_pos) printf("%s%s\n", ASNI_FMT("E*->",ASNI_FG_RED),iringbuf[j]);
+      else printf("    %s\n",iringbuf[j]);
     }
   }
 } 
