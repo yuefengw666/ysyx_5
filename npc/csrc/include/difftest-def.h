6d37f3ae920f545692219c2bc3d67e650981d0f3
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-22 09:51:05 +0800

    >  compile NEMU ysyx_22040237 吴岳峰 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:51:05 up 39 min,  1 user,  load average: 0.26, 0.40, 0.43

---
+++ npc/csrc/include/difftest-def.h
@@ -0,0 +1,11 @@
+#ifndef __DIFFTEST_DEF_H__
+#define __DIFFTEST_DEF_H__
+
+#include "npc_common.h"
+
+enum { DIFFTEST_TO_DUT, DIFFTEST_TO_REF };
+
+void init_difftest(char *ref_so_file, long img_size);
+void difftest_step(vaddr_t pc, vaddr_t npc);
+
+#endif
