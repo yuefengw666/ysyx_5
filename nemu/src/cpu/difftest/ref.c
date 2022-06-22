ac2a4c8efe34b42540f9d0605c8f5da2afbeba73
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-22 14:29:37 +0800

    >  compile NEMU ysyx_22040237 吴岳峰 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:29:37 up  5:17,  1 user,  load average: 1.99, 1.37, 1.13

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -40,8 +40,6 @@ void difftest_regcpy(void *dut, bool direction) {
         cpu.gpr[i] = reg_state->gpr[i];
       }
     }
-
-  printf("pc : %lx after init diff_init\n",cpu.pc);
 }
 
 void difftest_exec(uint64_t n) {
