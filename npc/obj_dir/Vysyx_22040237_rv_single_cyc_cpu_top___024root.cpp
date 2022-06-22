45ab55c81bbace1d14274fa2aff4978868926c4e
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-22 20:33:51 +0800

    >  sim RTL ysyx_22040237 吴岳峰 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:33:51 up 11:21,  1 user,  load average: 0.71, 0.54, 0.49

--- npc/obj_dir/Vysyx_22040237_rv_single_cyc_cpu_top___024root.cpp
+++ npc/obj_dir/Vysyx_22040237_rv_single_cyc_cpu_top___024root.cpp
@@ -413,7 +413,7 @@ VL_INLINE_OPT void Vysyx_22040237_rv_single_cyc_cpu_top___024root___combo__TOP__
                + vlSelf->ysyx_22040237_rv_single_cyc_cpu_top__DOT__op2_jump);
         vlSelf->ysyx_22040237_rv_single_cyc_cpu_top__DOT__rd_data 
             = (vlSelf->ysyx_22040237_rv_single_cyc_cpu_top__DOT__op1 
-               + vlSelf->ysyx_22040237_rv_single_cyc_cpu_top__DOT__op2);
+               - vlSelf->ysyx_22040237_rv_single_cyc_cpu_top__DOT__op2);
     } else {
         vlSelf->ysyx_22040237_rv_single_cyc_cpu_top__DOT__pc_jump_addr = 0ULL;
         vlSelf->ysyx_22040237_rv_single_cyc_cpu_top__DOT__rd_data = 0ULL;
