25917298e2b91adfe73bef14f68c83e0e11eb417
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-22 13:24:43 +0800

    >  compile NEMU ysyx_22040237 吴岳峰 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:24:43 up  4:12,  1 user,  load average: 1.59, 0.89, 0.79

--- nemu/include/cpu/ifetch.h
+++ nemu/include/cpu/ifetch.h
@@ -3,9 +3,7 @@
 #include <memory/vaddr.h>
 
 static inline uint32_t inst_fetch(vaddr_t *pc, int len) {
-  printf("pc:%ln\n",pc);
   uint32_t inst = vaddr_ifetch(*pc, len);
-  printf("22\n");
   (*pc) += len;
   return inst;
 }
