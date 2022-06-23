b05d975b2b0cd85f8fcd6596f208271f12402664
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-23 16:37:18 +0800

    >  sim RTL ysyx_22040237 吴岳峰 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:37:18 up  6:47,  1 user,  load average: 0.88, 0.58, 0.37

--- npc/csrc/monitor/monitor.cpp
+++ npc/csrc/monitor/monitor.cpp
@@ -81,9 +81,9 @@ void init_monitor(int argc, char *argv[]) {
     parse_args(argc,argv);
     
     img_size = load_img(img_file);
-
-    init_difftest("/home/yfwu/ysyx-workbench/nemu/build/riscv64-nemu-interpreter-so", img_size);//remove difftest_port
-    //init_difftest("diff_so_file", img_size);//remove difftest_port
+    diff_so_file = (char *)"/home/yfwu/ysyx-workbench/nemu/build/riscv64-nemu-interpreter-so";
+    //init_difftest("/home/yfwu/ysyx-workbench/nemu/build/riscv64-nemu-interpreter-so", img_size);//remove difftest_port
+    init_difftest("diff_so_file", img_size);//remove difftest_port
     
     init_sdb();
 }
