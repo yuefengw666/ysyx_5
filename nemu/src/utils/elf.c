6998450ddc95e68f4518e8865769c6eff3c107c3
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-27 18:34:25 +0800

    >  compile NEMU ysyx_22040237 吴岳峰 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:34:25 up  8:00,  1 user,  load average: 0.42, 0.53, 0.63

--- nemu/src/utils/elf.c
+++ nemu/src/utils/elf.c
@@ -119,6 +119,9 @@ static int func_dep = 0;//display blank
 static char blank[100]= {0};
 
 void ftrace(vaddr_t pc, vaddr_t dnpc, int pc_inst_opcode, int pc_inst_funct3){
+    for(int i=0; i<cnt_trace_func; i++){
+        printf("func name :%s, addr:%lx, size:%ld\n",elf_func_info[i].name, elf_func_info[i].addr,elf_func_info[i].size);
+    }
     int pc_func_index = -1;
     int dnpc_func_index = -1;
     //printf("trace func number: %d\n", cnt_trace_func);
