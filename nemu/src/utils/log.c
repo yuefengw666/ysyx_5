2e0a176e8f5efe5a52edf28698b4ff15478a5442
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-27 23:56:12 +0800

    >  compile NEMU ysyx_22040237 吴岳峰 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:56:12 up 13:21,  1 user,  load average: 0.78, 0.36, 0.37

--- nemu/src/utils/log.c
+++ nemu/src/utils/log.c
@@ -10,7 +10,7 @@ void init_log(const char *log_file) {
     Assert(fp, "Can not open '%s'", log_file);
     log_fp = fp;
   }
-  printf("test stdout whether write to log_file\n");
+  Log("test stdout whether write to log_file\n");
   Log("Log is written to %s", log_file ? log_file : "stdout");
 }
 
