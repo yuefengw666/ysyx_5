44e97495bced127f50b33777cc31e01c31dc05e0
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-28 22:19:14 +0800

    >  sim RTL ysyx_22040237 吴岳峰 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:19:14 up 13:00,  1 user,  load average: 1.43, 0.94, 0.61

--- npc/csrc/single_core_tb.cpp
+++ npc/csrc/single_core_tb.cpp
@@ -10,9 +10,9 @@ int main(int argc, char**argv, char** env){
 
     sim_init();
     npc_reset();
-    
-    init_monitor(argc,argv);
 
+    init_monitor(argc,argv);
+    dut->inst_in = pmem(dut->pc);
     
     #ifdef CONFIG_SDB
         npc_sdb_mainloop();
