37250759459d63d826524c52980b8cfd17161031
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-28 18:51:04 +0800

    >  sim RTL ysyx_22040237 吴岳峰 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:51:04 up  9:31,  1 user,  load average: 0.98, 1.06, 1.00

--- npc/vsrc/single_cyc/ysyx_22040237_idu.v
+++ npc/vsrc/single_cyc/ysyx_22040237_idu.v
@@ -80,7 +80,7 @@ assign src_j = { {43{imm_u_j[19]}}, imm_u_j[19], imm_u_j[7:0], imm_u_j[8], imm_u
 wire inst_dummy = !(|opcode);
 
 // addi: func3:000, opcode:0010011
-assign inst_addi = !opcode[0] & opcode[1] & ~opcode[2] & ~opcode[3] & opcode[4] & ~opcode[5] & ~opcode[6] & ~func3[0] & ~func3[1] & ~func3[2];
+assign inst_addi = opcode[0] & opcode[1] & ~opcode[2] & ~opcode[3] & opcode[4] & ~opcode[5] & ~opcode[6] & ~func3[0] & ~func3[1] & ~func3[2];
 //ebreak: 1110011
 assign inst_ebreak = opcode[0] & opcode[1] & ~opcode[2] & ~opcode[3] & opcode[4] & opcode[5] & opcode[6] & ~func3[0] & ~func3[1] & ~func3[2];
 
