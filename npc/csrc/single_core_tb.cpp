9e1f72b95ba0bbda6fcca70dec3b3eec9f10d041
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-28 22:58:23 +0800

    >  sim RTL ysyx_22040237 吴岳峰 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:58:23 up 13:39,  1 user,  load average: 0.80, 0.56, 0.53

--- npc/csrc/single_core_tb.cpp
+++ npc/csrc/single_core_tb.cpp
@@ -12,8 +12,8 @@ int main(int argc, char**argv, char** env){
     npc_reset();
 
     init_monitor(argc,argv);
-    dut->inst_in = pmem_read(dut->pc);
-    dut->eval();
+    //dut->inst_in = pmem_read(dut->pc);
+    //dut->eval();
     printf("after reset, inst_in:%x\n",dut->inst_in);
     #ifdef CONFIG_SDB
         npc_sdb_mainloop();
