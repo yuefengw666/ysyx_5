504026d8ddc88e422375d0f47c7189047135df87
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-28 17:24:36 +0800

    >  sim RTL ysyx_22040237 吴岳峰 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:24:36 up  8:05,  1 user,  load average: 1.00, 0.52, 0.47

---
+++ npc/build/obj_dir/Vysyx_22040237_rv_single_cyc_cpu_top___024root.h
@@ -0,0 +1,76 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Design internal header
+// See Vysyx_22040237_rv_single_cyc_cpu_top.h for the primary calling header
+
+#ifndef VERILATED_VYSYX_22040237_RV_SINGLE_CYC_CPU_TOP___024ROOT_H_
+#define VERILATED_VYSYX_22040237_RV_SINGLE_CYC_CPU_TOP___024ROOT_H_  // guard
+
+#include "verilated_heavy.h"
+
+//==========
+
+class Vysyx_22040237_rv_single_cyc_cpu_top__Syms;
+class Vysyx_22040237_rv_single_cyc_cpu_top_VerilatedVcd;
+class Vysyx_22040237_rv_single_cyc_cpu_top___024unit;
+
+
+//----------
+
+VL_MODULE(Vysyx_22040237_rv_single_cyc_cpu_top___024root) {
+  public:
+    // CELLS
+    Vysyx_22040237_rv_single_cyc_cpu_top___024unit* __PVT____024unit;
+
+    // PORTS
+    VL_IN8(clk,0,0);
+    VL_IN8(rst,0,0);
+    VL_IN(inst_in,31,0);
+    VL_OUT64(pc,63,0);
+    VL_OUT64(rs2_data,63,0);
+
+    // LOCAL SIGNALS
+    CData/*0:0*/ ysyx_22040237_rv_single_cyc_cpu_top__DOT__inst_ebreak;
+    CData/*7:0*/ ysyx_22040237_rv_single_cyc_cpu_top__DOT__inst_opcode;
+    CData/*0:0*/ ysyx_22040237_rv_single_cyc_cpu_top__DOT__rs1_r_en;
+    CData/*4:0*/ ysyx_22040237_rv_single_cyc_cpu_top__DOT__rs1_r_addr;
+    CData/*0:0*/ ysyx_22040237_rv_single_cyc_cpu_top__DOT__rd_w_en;
+    CData/*4:0*/ ysyx_22040237_rv_single_cyc_cpu_top__DOT__rd_w_addr;
+    CData/*5:0*/ ysyx_22040237_rv_single_cyc_cpu_top__DOT__ysyx_22040237_idu_u0__DOT__inst_type;
+    CData/*0:0*/ ysyx_22040237_rv_single_cyc_cpu_top__DOT__ysyx_22040237_idu_u0__DOT__inst_addi;
+    CData/*0:0*/ ysyx_22040237_rv_single_cyc_cpu_top__DOT__ysyx_22040237_idu_u0__DOT__inst_auipc;
+    CData/*0:0*/ ysyx_22040237_rv_single_cyc_cpu_top__DOT__ysyx_22040237_idu_u0__DOT__inst_lui;
+    CData/*0:0*/ ysyx_22040237_rv_single_cyc_cpu_top__DOT__ysyx_22040237_idu_u0__DOT__inst_jal;
+    CData/*0:0*/ ysyx_22040237_rv_single_cyc_cpu_top__DOT__ysyx_22040237_idu_u0__DOT__inst_jalr;
+    QData/*63:0*/ ysyx_22040237_rv_single_cyc_cpu_top__DOT__rs1_data;
+    QData/*63:0*/ ysyx_22040237_rv_single_cyc_cpu_top__DOT__op1;
+    QData/*63:0*/ ysyx_22040237_rv_single_cyc_cpu_top__DOT__op2;
+    QData/*63:0*/ ysyx_22040237_rv_single_cyc_cpu_top__DOT__op1_jump;
+    QData/*63:0*/ ysyx_22040237_rv_single_cyc_cpu_top__DOT__op2_jump;
+    QData/*63:0*/ ysyx_22040237_rv_single_cyc_cpu_top__DOT__rd_data;
+    QData/*63:0*/ ysyx_22040237_rv_single_cyc_cpu_top__DOT__pc_jump_addr;
+    QData/*63:0*/ ysyx_22040237_rv_single_cyc_cpu_top__DOT__ysyx_22040237_idu_u0__DOT__src_i;
+    VlUnpacked<QData/*63:0*/, 32> ysyx_22040237_rv_single_cyc_cpu_top__DOT__ysyx_22040237_reg_file_u0__DOT__regs;
+    VlUnpacked<QData/*63:0*/, 33> ysyx_22040237_rv_single_cyc_cpu_top__DOT__ysyx_22040237_reg_file_u0__DOT__rf;
+
+    // LOCAL VARIABLES
+    CData/*0:0*/ __Vclklast__TOP__clk;
+    VlUnpacked<CData/*0:0*/, 3> __Vm_traceActivity;
+
+    // INTERNAL VARIABLES
+    Vysyx_22040237_rv_single_cyc_cpu_top__Syms* vlSymsp;  // Symbol table
+
+    // CONSTRUCTORS
+  private:
+    VL_UNCOPYABLE(Vysyx_22040237_rv_single_cyc_cpu_top___024root);  ///< Copying not allowed
+  public:
+    Vysyx_22040237_rv_single_cyc_cpu_top___024root(const char* name);
+    ~Vysyx_22040237_rv_single_cyc_cpu_top___024root();
+
+    // INTERNAL METHODS
+    void __Vconfigure(Vysyx_22040237_rv_single_cyc_cpu_top__Syms* symsp, bool first);
+} VL_ATTR_ALIGNED(VL_CACHE_LINE_BYTES);
+
+//----------
+
+
+#endif  // guard
