65361b1b27e521ec44bd738c5bbdaf4270ad271c
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-07 18:23:19 +0800

    >  sim RTL ysyx_22040237 吴岳峰 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:23:19 up  3:06,  1 user,  load average: 0.33, 0.49, 0.51

--- npc/vsrc/single_cyc/ysyx_22040237_exu.v
+++ npc/vsrc/single_cyc/ysyx_22040237_exu.v
@@ -289,7 +289,9 @@ assign divw_res = { {32{divw_res_t[31]}}, divw_res_t};
 //divuw
 wire wop_divuw = mdu_req && exu_info_bus_i[`ysyx_22040237_EXU_INFO_MDU_DIVU] && mdu_wop;
 wire [`ysyx_22040237_REG_WIDTH-1:0] divuw_res;
+/* verilator lint_off UNUSED */
 wire [63:0] divuw_res_t = ($unsigned(op1_i)) / ($unsigned(op2_i));
+/* verilator lint_on UNUSED */
 assign divuw_res = { {32{divuw_res_t[31]}}, divuw_res_t[31:0]};
 
 //remw
@@ -301,7 +303,9 @@ assign remw_res = { {32{remw_res_t[31]}}, remw_res_t};
 //remuw
 wire wop_remuw = mdu_req && exu_info_bus_i[`ysyx_22040237_EXU_INFO_MDU_REMU] && mdu_wop;
 wire [`ysyx_22040237_REG_WIDTH-1:0] remuw_res;
+/* verilator lint_off UNUSED */
 wire [63:0] remuw_res_t = ($unsigned(op1_i)) % ($unsigned(op2_i));
+/* verilator lint_on UNUSED */
 assign remuw_res = { {32{remuw_res_t[31]}}, remuw_res_t[31:0]};
 
 
