0cd4eb50d61814f9d1bd62282ca906aa6a6e92ff
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-15 00:02:50 +0800

    >  compile NEMU ysyx_22040237 吴岳峰 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:02:50 up 17:49,  1 user,  load average: 0.55, 0.52, 0.72

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -5,8 +5,6 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 static unsigned long int next = 1;
 
-static void *addr = NULL;
-
 int rand(void) {
   // RAND_MAX assumed to be 32767
   next = next * 1103515245 + 12345;
@@ -31,17 +29,32 @@ int atoi(const char* nptr) {
   return x;
 }
 
+bool is_heap_reset = false;
+void *heapaddr;
+
+inline void heap_reset() {
+  heapaddr = (void*)heap.start;
+  is_heap_reset = true;
+}
+
 void *malloc(size_t size) {
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
   //panic("Not implemented");
+  /*
   if(!addr) addr = heap.start;
   void *addr_p = addr;
   addr += size + 32;
+  */
+  if (!is_heap_reset) heap_reset();
+  size = (size_t)ROUNDUP(size, 8);
+  void *p = heapaddr;
+  heapaddr += size;
+  return p;
 #endif
-  return addr_p;
+  return NULL;
 }
 
 void free(void *ptr) {
