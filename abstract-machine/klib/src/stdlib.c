27f4f2837923d21e3041adc5015a5e3dd436c6eb
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-14 22:33:28 +0800

    >  compile NEMU ysyx_22040237 吴岳峰 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:33:28 up 16:19,  1 user,  load average: 0.87, 0.34, 0.24

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -5,6 +5,8 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 static unsigned long int next = 1;
 
+static void *addr = NULL;
+
 int rand(void) {
   // RAND_MAX assumed to be 32767
   next = next * 1103515245 + 12345;
@@ -34,9 +36,12 @@ void *malloc(size_t size) {
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  panic("Not implemented");
+  //panic("Not implemented");
+  if(!addr) addr = heap.start;
+  void *addr_p = addr;
+  addr += size + 32;
 #endif
-  return NULL;
+  return addr_p;
 }
 
 void free(void *ptr) {
