7859fbc0137cee5ce94bbf7b24b906215ec882e2
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-22 01:57:47 +0800

    >  compile NEMU ysyx_22040237 吴岳峰 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:57:47 up  4:37,  1 user,  load average: 1.04, 0.89, 0.85

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -62,7 +62,7 @@ void paddr_write(paddr_t addr, int len, word_t data) {
     }
   #endif
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
-  printf("befor mmio_write addr: %xn",addr);
+  printf("befor mmio_write addr: %x\n",addr);
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
 }
