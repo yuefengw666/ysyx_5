a12931f1d688c4194489f11b6ed94582d0492a87
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-22 01:00:09 +0800

    >  compile NEMU ysyx_22040237 吴岳峰 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:00:09 up  3:39,  1 user,  load average: 2.07, 1.14, 0.87

--- nemu/src/device/io/mmio.c
+++ nemu/src/device/io/mmio.c
@@ -29,4 +29,5 @@ word_t mmio_read(paddr_t addr, int len) {
 void mmio_write(paddr_t addr, int len, word_t data) {
   printf("111\n");
   map_write(addr, len, data, fetch_mmio_map(addr));
+  printf("222\n");
 }
