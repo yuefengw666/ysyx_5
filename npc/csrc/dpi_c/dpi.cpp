b4c69659e156e6ee2a4e65110ddf3a1b6c5f536e
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-22 02:58:32 +0800

    >  sim RTL ysyx_22040237 吴岳峰 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  02:58:32 up  5:38,  1 user,  load average: 0.76, 0.69, 0.56

--- npc/csrc/dpi_c/dpi.cpp
+++ npc/csrc/dpi_c/dpi.cpp
@@ -74,7 +74,7 @@ if( (waddr >= CONFIG_MBASE) && (waddr < CONFIG_MBASE + CONFIG_MSIZE) ){
   #ifdef CONFIG_HAS_UART
     if(waddr == SERIAL_ADDR) {
       char b[100];
-      snprintf(b, 8, "%lld", wdata);
+      snprintf(b, 8, "%s", wdata);
       printf("%s\n", b);
       return;
     }
