17b39076c2120c2e2c9dad373a49fb1279a0435f
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-22 16:23:17 +0800

    >  sim RTL ysyx_22040237 吴岳峰 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:23:17 up  6:51,  1 user,  load average: 1.50, 1.19, 0.99

--- abstract-machine/am/src/riscv/npc/timer.c
+++ abstract-machine/am/src/riscv/npc/timer.c
@@ -1,16 +1,17 @@
 #include <am.h>
 #include <riscv/riscv.h>
-
+#include <stdio.h>
 #define RTC_ADDR   0xa0000048
 
 void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  //uint32_t hi = inl(RTC_ADDR + 4);
-  //uint32_t lo = inl(RTC_ADDR);
-  //uint64_t time = ( (uint64_t)hi << 32 ) | lo;
-  uptime->us = 0;
+  uint32_t hi = inl(RTC_ADDR + 4);
+  uint32_t lo = inl(RTC_ADDR);
+  uint64_t time = ( (uint64_t)hi << 32 ) | lo;
+  uptime->us = time;
+  printf("uptime: %lx\n",time);
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
