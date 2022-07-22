e829cf7a8ab1cca2dc9bf47ec46f6492d483fa89
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-22 12:38:40 +0800

    >  sim RTL ysyx_22040237 吴岳峰 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:38:40 up  3:07,  1 user,  load average: 1.30, 1.07, 0.98

--- npc/csrc/utils/timer.cpp
+++ npc/csrc/utils/timer.cpp
@@ -10,15 +10,15 @@
 static uint64_t boot_time = 0;
 
 static uint64_t get_time_internal() {
-  /*
   struct timeval now;
   gettimeofday(&now, NULL);
   uint64_t us = now.tv_sec * 1000000 + now.tv_usec;
-  */
+ /*
   struct timespec now;
   clock_gettime(CLOCK_MONOTONIC_COARSE, &now);
   uint64_t us = now.tv_sec * 1000000 + now.tv_nsec / 1000;
   return us;
+  */
 }
 
 uint64_t get_time() {
