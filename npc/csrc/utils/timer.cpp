78cad51c89e1b70c01716b7f53392bf3f69d3005
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-22 15:37:57 +0800

    >  sim RTL ysyx_22040237 吴岳峰 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:37:57 up  6:06,  1 user,  load average: 0.82, 0.38, 0.19

--- npc/csrc/utils/timer.cpp
+++ npc/csrc/utils/timer.cpp
@@ -1,8 +1,8 @@
 #include <npc_common.h>
 
 #ifdef CONFIG_HAS_TIMER
-    //#include <sys/time.h>
-    #include <time.h>
+    #include <sys/time.h>
+    //#include <time.h>
 
 static_assert(CLOCKS_PER_SEC == 1000000, "CLOCKS_PER_SEC != 1000000");
 static_assert(sizeof(clock_t) == 8, "sizeof(clock_t) != 8");
